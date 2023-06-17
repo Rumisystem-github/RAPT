0dd60dbd9ab03ff260a115d9f0a440a1250d1088
るみさん <irjitgd@gmail.com>
2023-06-17 17:46:23 +0900

    ヘルプこまんぢ実装 ・ヘルプコマンド ・UPDATEするのを関数化

--- SRC/Main.cpp
+++ SRC/Main.cpp
@@ -11,42 +11,73 @@
 #include <iostream>
 #include "ENV.h"
 
+void HELP(){
+	PRINT("┌─[RAPTコマンド ヘルプ]");
+	PRINT("│update/-Syy");
+	PRINT("│→パッケージリストを更新します");
+	PRINT("│upgrade/-Syyu");
+	PRINT("│→パッケージを更新します");
+	PRINT("│");
+	PRINT("│");
+	PRINT("│");
+	PRINT("│");
+}
+
+void UPDATE(String SETTING){
+	String REQ_URI = JSON_PARSE("URI", SETTING);//リクエスト先URI
+
+	PRINT("[ *** ]GET:" + REQ_URI);
+	String REPO_INDEX_DATA = HTTP_REQUEST(REQ_URI + "/index.json");//リクエスト送信
+	if(REPO_INDEX_DATA != ""){//エラーチェック
+		//成功
+		PRINT("[ OK ]GET:" + REQ_URI);
+
+		//リポジトリの設定情報を解析
+		String REPO_INDEX_PLU = JSON_PARSE("PAC_LIST_URI", REPO_INDEX_DATA);//パッケージリストのリクエスト先
+		PRINT("[ LOG ]PAC LIST URI:" + REPO_INDEX_PLU);
+		String REPO_INDEX_PU = JSON_PARSE("PAC_URI", REPO_INDEX_DATA);//パッケージ本体の取得先
+		PRINT("[ LOG ]PAC URI:" + REPO_INDEX_PLU);
+
+		//パッケージリストを取得
+		PRINT("[ *** ]GET:" + REQ_URI + "/" + REPO_INDEX_PLU);
+		String REPO_LIST_DATA = HTTP_REQUEST(REQ_URI + "/" + REPO_INDEX_PLU);//リクエスト送信
+		if(REPO_LIST_DATA != ""){//エラーチェック
+			//成功
+			PRINT("[ OK ]" + std::to_string(JSON_PARSE_ARRAY_COUNT(REPO_LIST_DATA)) + "件のパッケージが見つかりました");
+			PRINT("[ *** ]アップデートのチェックをしています。。。");
+		}else{
+			//エラー
+			PRINT("[ ERR ]失敗 GET:" + REQ_URI + "/" + REPO_INDEX_PLU);
+			PRINT("要求したサーバーは、index.jsonの設定がしっかりとされていません。\n管理者に問い合わせてください");
+			return 1;
+		}
+	}else{
+		//エラー
+		PRINT("[ ERR ]失敗 GET:" + REQ_URI + "/index.json");
+		PRINT("要求したサーバーは、本当にRAPTのリポジトリサーバーですか？確認してください。");
+		return 1;
+	}
+}
+
 int main(int argc, char* argv[]){
+	if(argc <= 1){
+		HELP();
+		return 0;
+	}
+	
 	//mainはintでやる決まりでも有るんかな
 	String SETTING = FILE_GET("./BIN/SETTING.json");//設定ファイルを読み込み
 	if(SETTING != ""){//エラーチェック
-		String REQ_URI = JSON_PARSE("URI", SETTING);//リクエスト先URI
+		switch (argv[1][0]){//コマンドライン引数をチェック
+			case 'u':
+				if (std::string(argv[1]) == "update") {
+					PRINT("[ *** ]パッケージリストを更新します...");
+					UPDATE(SETTING);
+				}
+				break;
 
-		PRINT("[ *** ]GET:" + REQ_URI);
-		String REPO_INDEX_DATA = HTTP_REQUEST(REQ_URI + "/index.json");//リクエスト送信
-		if(REPO_INDEX_DATA != ""){//エラーチェック
-			//成功
-			PRINT("[ OK ]GET:" + REQ_URI);
-
-			//リポジトリの設定情報を解析
-			String REPO_INDEX_PLU = JSON_PARSE("PAC_LIST_URI", REPO_INDEX_DATA);//パッケージリストのリクエスト先
-			PRINT("[ LOG ]PAC LIST URI:" + REPO_INDEX_PLU);
-			String REPO_INDEX_PU = JSON_PARSE("PAC_URI", REPO_INDEX_DATA);//パッケージ本体の取得先
-			PRINT("[ LOG ]PAC URI:" + REPO_INDEX_PLU);
-
-			//パッケージリストを取得
-			PRINT("[ *** ]GET:" + REQ_URI + "/" + REPO_INDEX_PLU);
-			String REPO_LIST_DATA = HTTP_REQUEST(REQ_URI + "/" + REPO_INDEX_PLU);//リクエスト送信
-			if(REPO_LIST_DATA != ""){//エラーチェック
-				//成功
-				PRINT("[ OK ]" + std::to_string(JSON_PARSE_ARRAY_COUNT(REPO_LIST_DATA)) + "件のパッケージが見つかりました");
-				PRINT("[ *** ]アップデートのチェックをしています。。。");
-			}else{
-				//エラー
-				PRINT("[ ERR ]失敗 GET:" + REQ_URI + "/" + REPO_INDEX_PLU);
-				PRINT("要求したサーバーは、index.jsonの設定がしっかりとされていません。\n管理者に問い合わせてください");
-				return 1;
-			}
-		}else{
-			//エラー
-			PRINT("[ ERR ]失敗 GET:" + REQ_URI + "/index.json");
-			PRINT("要求したサーバーは、本当にRAPTのリポジトリサーバーですか？確認してください。");
-			return 1;
+			default:
+				break;
 		}
 	}else{
 		PRINT("設定ファイルが異常です、確認してください");
